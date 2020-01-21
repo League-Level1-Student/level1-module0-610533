3b255bbefc3544a110c26c14b30f581e6f9592c9
610533 <610533@0338S-S-R0B6ZHA.sdcs.local>
2020-01-21 14:25:10 -0800

    houses

--- src/_01_houses/Houses.java
+++ src/_01_houses/Houses.java
@@ -1,7 +1,13 @@
 package _01_houses;
-
+import org.jointheleague.graphical.robot.Robot;
 public class Houses {
 	public void run() {
-		
+			Robot Rob = new Robot();
+			Rob.setX(-99);
+			Rob.setY(-99);
+			Rob.penDown();
+			Rob.move(100);
+			Rob.turn(180/2);
+			Rob.move(50);
+		}
 	}
-}
