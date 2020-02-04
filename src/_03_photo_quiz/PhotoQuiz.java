ebe06feb8fb93d80e8e20fedd0050253185e9aad
610533 <610533@0338S-S-R0B6ZHA.sdcs.local>
2020-02-04 14:23:34 -0800

    photoquiz done, drumkit partially done

--- src/_03_photo_quiz/PhotoQuiz.java
+++ src/_03_photo_quiz/PhotoQuiz.java
@@ -13,6 +13,7 @@ import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 
 public class PhotoQuiz {
 
@@ -25,35 +26,50 @@ public class PhotoQuiz {
 
 		// 1. find an image on the internet, and put its URL in a String
 		// variable (from your browser, right click on the image, and select
-		// “Copy Image Address”)
-
+		// â€œCopy Image Addressâ€�)
+		String bruh = ("https://i.ytimg.com/vi/s_htF2fKyGs/maxresdefault.jpg");
 		// 2. create a variable of type "Component" that will hold your image
-
+		Component f = createImage(bruh);
 		// 3. use the "createImage()" method below to initialize your Component
-
+		
 		// 4. add the image to the quiz window
-
+		JPanel panel = new JPanel();
+		quizWindow.add(panel);
+		panel.add(f);
 		// 5. call the pack() method on the quiz window
-
+		quizWindow.pack();
 		// 6. ask a question that relates to the image
-
+		String lol = JOptionPane.showInputDialog("What is this?");
 		// 7. print "CORRECT" if the user gave the right answer
-
+		if(lol.equalsIgnoreCase("Phrog")) {
+		JOptionPane.showMessageDialog(null, "Correct!");
+		}
 		// 8. print "INCORRECT" if the answer is wrong
-
+		else {
+			JOptionPane.showMessageDialog(null, "WRONG!!! HOW COULD YOU NOT GET THAT?! HAHAHAHAHAHAHAHAHHAAAA!!!!!AHAHHAAHAHAHAHAHAA!!!");
+		}
 		// 9. remove the component from the quiz window (you may not see the
 		// effect of this until step 12)
-
+		quizWindow.remove(panel);
 		// 10. find another image and create it (might take more than one line
 		// of code)
-
+		String hmm = ("https://upload.wikimedia.org/wikipedia/commons/a/a3/June_odd-eyed-cat.jpg");
+		Component b = createImage(hmm);
 		// 11. add the second image to the quiz window
-
+		quizWindow.add(b);
 		// 12. pack the quiz window
-
+		quizWindow.pack();
 		// 13. ask another question
-
+		String haha = JOptionPane.showInputDialog("What is this though?");
 		// 14+ check answer, say if correct or incorrect, etc.
+		if(haha.equalsIgnoreCase("cat")) {
+			JOptionPane.showMessageDialog(null, "Correct!");
+		}
+		else {
+			JOptionPane.showMessageDialog(null, "WRONG!!! HOW COULD YOU NOT GET THAT?! HAHAHAHAHAHAHAHAHHAAAA!!!!!AHAHHAAHAHAHAHAHAA!!!");
+			
+		}
+		quizWindow.remove(b);
 
 	}
 
